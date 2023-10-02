35f9503061afc4bcbfd19e274046f556b4110053
Samdine Murray <eaglesamdine@gmail.com>
2023-10-02 16:18:33 -0400

    Sprint 2: Created Test File

---
+++ where-to-live-app/src/TEST.java
@@ -0,0 +1,6 @@
+public class TEST {
+    public static void main(String[] args) {
+        main m = new main();
+        m.runStuff();
+    }
+}
--- where-to-live-app/src/main.java
+++ where-to-live-app/src/main.java
@@ -1,5 +1,10 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Running stuff.");
+    }
+
+    public void runStuff() {
+        int i = 3 * 4;
+        System.out.println("Number: " + i);
     }
 }
